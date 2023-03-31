ebc7f5f6ed6d43ff84b5c3f2feeb78caa317a22c
Yimin ZHOU <55327029+Yimin-zhou@users.noreply.github.com>
2023-03-31 21:22:34 +0200

    Update particle_frag.glsl

--- shaders/particle_frag.glsl
+++ shaders/particle_frag.glsl
@@ -4,11 +4,12 @@ layout(location = 4) uniform sampler2D particleMap;
 in vec3 fragPosition;
 in vec3 fragNormal;
 in vec2 fragTexCoord;
+in vec4 particleColor;
 
 layout(location = 0) out vec4 fragColor;
 
 void main()
 {
     const vec3 emissiveColor = vec3(0.97, 0.21, 0.2);
-	fragColor = vec4(emissiveColor, 1.0);
+	fragColor = vec4(vec3(particleColor), 1.0);
 }
